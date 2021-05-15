051d89ccb36a4dab8f236a26fa903a40498b7c7e
sauravbaldwin <41564194+sauravbaldwin@users.noreply.github.com>
2021-05-16 03:32:48 +0530

    Add files via upload

---
+++ Disable_Blob_PublicAccess.ps1
@@ -0,0 +1,6 @@
+﻿$storages = Get-Content -path Desktop\storage_nonprod.csv
+foreach ($storage in $storages)
+{
+$resource=Get-AzResource -Name $storage
+Set-AzStorageAccount -ResourceGroupName $resource.ResourceGroupName -Name $resource.Name -AllowBlobPublicAccess $false
+}
